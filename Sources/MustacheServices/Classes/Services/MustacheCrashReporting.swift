fd6882182f548a929ca49daf1a58c8c1c0efb9b3
Tommy Sadiq Hinrichsen <th@mustache.dk>
2019-11-25 12:15:14 +0100

    Removed crashreporting

--- Sources/MustacheServices/Classes/Services/MustacheCrashReporting.swift
+++ Sources/MustacheServices/Classes/Services/MustacheCrashReporting.swift
@@ -5,22 +5,22 @@
 //  Created by Tommy Hinrichsen on 09/10/2019.
 //
 
-import Foundation
-import KSCrash
-
-public class MustacheCrashReporting {
-
-    public static let shared = MustacheCrashReporting()
-
-    private let installation: KSCrashInstallationStandard
-
-    private init(){
-        self.installation = KSCrashInstallationStandard.sharedInstance()
-        self.installation.url = URL(string: "https://mustache-services.herokuapp.com/api/v1/crashreporting")
-        self.installation.install()
-
-        KSCrash.sharedInstance()?.deleteBehaviorAfterSendAll = KSCDeleteOnSucess
-        self.installation.sendAllReports { (reports, completed, error) -> Void in }
-    }
-
-}
+// import Foundation
+// import KSCrash
+//
+// public class MustacheCrashReporting {
+//
+//     public static let shared = MustacheCrashReporting()
+//
+//     private let installation: KSCrashInstallationStandard
+//
+//     private init(){
+//         self.installation = KSCrashInstallationStandard.sharedInstance()
+//         self.installation.url = URL(string: "https://mustache-services.herokuapp.com/api/v1/crashreporting")
+//         self.installation.install()
+//
+//         KSCrash.sharedInstance()?.deleteBehaviorAfterSendAll = KSCDeleteOnSucess
+//         self.installation.sendAllReports { (reports, completed, error) -> Void in }
+//     }
+//
+// }

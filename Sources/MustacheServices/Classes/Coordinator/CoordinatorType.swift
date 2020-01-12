ee7e5bedeeab8d913d5e7fec7fac3d1eaae9debe
Tommy Sadiq Hinrichsen <th@mustache.dk>
2020-01-12 14:28:57 +0100

    Removed methods from coordinator

--- Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
+++ Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
@@ -3,13 +3,6 @@ import UIKit
 // https://medium.com/concretelatinoamérica/inverse-reference-coordinator-pattern-d5a5948c0d90
 public protocol CoordinatorType: NSObjectProtocol {
 
-    /// Should be a weak property to prevent memory leaks
-    var rootController: UIViewController? { get }
-
-    var parentCoordinator: CoordinatorType! { get set }
-    
-    init(parent: CoordinatorType) 
-    
     func start() throws
     
     func stop() throws

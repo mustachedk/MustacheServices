6d8c5e6233a4e917b139829bdfc4a9e79981f37f
Tommy Sadiq Hinrichsen <th@mustache.dk>
2020-01-12 10:45:16 +0100

    Inverse Reference Coordinator Pattern

--- Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
+++ Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
@@ -1,16 +1,28 @@
 import UIKit
 
+// https://medium.com/concretelatinoamérica/inverse-reference-coordinator-pattern-d5a5948c0d90
 public protocol CoordinatorType: NSObjectProtocol {
 
+    /// Should be a weak property to prevent memory leaks
     var rootController: UIViewController? { get }
 
-    var parentCoordinator: CoordinatorType? { get set }
+    var parentCoordinator: CoordinatorType! { get set }
+    
+    init(parent: CoordinatorType) 
     
     func start() throws
+    
+    func stop() throws
 
     func transition(to transition: Transition) throws
 
 
+}
+
+extension CoordinatorType {
+    
+    func stop() throws { }
+    
 }
 
 public protocol Transition {

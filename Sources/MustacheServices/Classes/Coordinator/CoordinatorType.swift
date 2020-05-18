561ed75bbf563bcccb1bee5c2e77662a950d0d1d
Tommy Sadiq Hinrichsen <th@mustache.dk>
2020-05-18 14:33:27 +0200

    Added route

--- Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
+++ Sources/MustacheServices/Classes/Coordinator/CoordinatorType.swift
@@ -4,20 +4,25 @@ import UIKit
 public protocol CoordinatorType: NSObjectProtocol {
 
     func start() throws
-    
+
     func stop() throws
 
     func transition(to transition: Transition) throws
 
+    func route(to: route: Route)
 
 }
 
 public extension CoordinatorType {
-    
+
     func stop() throws { }
-    
+
 }
 
 public protocol Transition {
+
+}
+
+public protocol Route {
 
 }

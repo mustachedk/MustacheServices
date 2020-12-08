1e84d80275b643b75f2778791b0945ea57d20c96
Tommy Sadiq Hinrichsen <tommy.lynge@gmail.com>
2020-12-08 12:01:51 +0100

    Added extensions from sengros

---
+++ Sources/MustacheServices/Extensions/CLLocation+Extensions.swift
@@ -0,0 +1,35 @@
+
+import Foundation
+import CoreLocation
+import MapKit
+
+public extension CLLocation {
+
+    func movedBy(latitudinalMeters: CLLocationDistance, longitudinalMeters: CLLocationDistance) -> CLLocation {
+
+        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: abs(latitudinalMeters), longitudinalMeters: abs(longitudinalMeters))
+
+        let latitudeDelta = region.span.latitudeDelta
+        let longitudeDelta = region.span.longitudeDelta
+
+        let latitudialSign = CLLocationDistance(latitudinalMeters.sign == .minus ? -1 : 1)
+        let longitudialSign = CLLocationDistance(longitudinalMeters.sign == .minus ? -1 : 1)
+
+        let newLatitude = coordinate.latitude + latitudialSign * latitudeDelta
+        let newLongitude = coordinate.longitude + longitudialSign * longitudeDelta
+
+        let newCoordinate = CLLocationCoordinate2D(latitude: newLatitude, longitude: newLongitude)
+
+        let newLocation = CLLocation(coordinate: newCoordinate, altitude: altitude, horizontalAccuracy: horizontalAccuracy, verticalAccuracy: verticalAccuracy, course: course, speed: speed, timestamp: Date())
+
+        return newLocation
+    }
+}
+
+public extension CLLocationCoordinate2D {
+
+    func movedBy(latitudinalMeters: CLLocationDistance, longitudinalMeters: CLLocationDistance) -> CLLocation {
+        let location = CLLocation(coordinate: self)
+        return location.movedBy(latitudinalMeters: latitudinalMeters, longitudinalMeters: longitudinalMeters)
+    }
+}

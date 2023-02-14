12e968cbd2dbba01c707b85675cf091e5fa0d7c8
Tommy Sadiq Hinricshen <th@mustache.dk>
2023-02-14 12:44:37 +0100

    Possible to use KeychainItemAccessibility when storing items in the keychain

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
      .package(url: "https://github.com/hmlongco/Resolver.git", .upToNextMajor(from: "1.4.1")),
-     .package(url: "https://github.com/mustachedk/MustacheFoundation.git", .upToNextMajor(from: "2.1.2"))
+     .package(url: "https://github.com/mustachedk/MustacheFoundation.git", .upToNextMajor(from: "2.2.0"))
   ],
   targets: [
     .target(name: "MustacheServices",dependencies: ["Resolver", "MustacheFoundation"])
